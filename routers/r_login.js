39d5c86e6035aec34e90ee40a99b0cd04814f5c9
privatehobby <darkbast@naver.com>
2018-04-18 01:05:29 +0900

    routers view up

---
+++ routers/r_login.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+router.get('/',(req,res,next) => {
+    console.log('/login get');
+ res.sendFile(path.join(__dirname,'/..','views','html','login','login.html'));
+});
+
+router.post('/',(req,res,next) => {
+    console.log('/login post');
+    console.log(req.body);
+    res.sendFile(path.join(__dirname,'/..','views','html','login'));
+});
+
+router.post('/check',(req,res,next) => {
+    
+});
+
+
+module.exports = router;
